f53888c8419c777a90d5b8fc0dcef7ce9d592a1e
huy12211221 <thanhhuy12211331@gmail.com>
2022-01-05 16:29:26 +0700

    2

--- Thực hành trên bộ dữ liệu Online Retail/past1.py
+++ Thực hành trên bộ dữ liệu Online Retail/past1.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 data_frame = pd.read_csv("D:\Data Analytics\Thực hành trên bộ dữ liệu Online Retail\OnlineRetail.csv", encoding="ISO-8859-1")
 
-data_frame.info()
+#data_frame.info()
+#print(data_frame.Country.value_counts())
+country = data_frame.Country.unique()
+print(str(country.size))
